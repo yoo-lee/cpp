684c9dc8d7b02d5935f834ff63146fcf029e99e3
yoo-lee <83053630+yoo-lee@users.noreply.github.com>
2022-08-17 21:54:26 +0900

    2

--- megaphone.cpp
+++ megaphone.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
+using namespace std;
 
-int main (int ac, char **av)
+int main (int ac, char **c)
 {
 	int i = 0;
-	char c = '\0';
+	char n;
+	cout << "\"" << endl;
 
-		printf("1\n");
+	if (ac == 1)
+	cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
 	if (ac == 2)
-	std::cout << "" << std::endl;
-	if (ac == 3)
-	std::cout << "" << std::endl;
-	
-	if (ac == 4 || av[4][i])
+	cout << "shhhhh... I think the students are asleep..." << endl;
+	if (ac == 4)
+	// cout << "Damnit" "/"\" ! "\"\" ""Sorry students, I thought this thing was off."" << endl; 
+
+//  ./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
+// DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.
+
+	printf("21\n");
+	if (ac == 4 || c[4][i])
 	{
-		printf("%c", av[4][i]);
-		i++;
-		c = toupper(av[4][i]);
-		std::cout << c << std::endl;
+		n = toupper(c[4][i]);
+	cout << "20\n";
+		cout << n << endl;
 		return 0;
 	}
 	//toupper to be make upper words by one character
-
-	std::cout << c << std::endl;
+	// cout << c << endl;
 	return 0;
 }
---
+++ study/cout.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+int main ()
+{
+	int n;
+	n = 0;
+
+	// cout << "";
+	cin >> n;//cin 標準入力
+	cout << n << endl << "you" << endl;
+	return 0;
+}
