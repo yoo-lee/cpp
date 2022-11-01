d10e132f85a2008d6e2d1041806c61078669d6d5
root <root@DESKTOP-OKSHI5U.localdomain>
2022-11-01 19:58:49 +0900

    2

---
+++ megaphone.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+
+char ft_toupper(char s)
+{
+		return (s -('Z' - 'z'));
+}
+
+int main (int ac, char **av)
+{
+		int i,j,m;
+		i = 1;
+		j = 0;
+
+		std::string s[i];
+		m = strlen(*av);
+	if (ac == 1)
+		std::cout << "" << std::endl;
+
+		while(i < ac)
+		{
+		std::cout << av[i] << std::endl;
+		i++;
+		}
+	// if (ac == 2)
+	// {
+	// 	while (*av)
+	// 	{
+	// 		s[i] = ft_toupper(*av[j]);
+	// 		i++;
+	// 		j++;
+	// 		std::cout << s[i];
+	// 	}
+	// }
+}
--- study/select/ac.cpp
+++ study/select/ac.cpp
@@ -6,15 +6,17 @@ using namespace std;
 
 int main (int ac , char **av)
 {
-	std::string str; 
-	std::string up_str; 
+	std::string str;
+	std::string up_str;
 	int i , j;
 	i = 1;
 	j = 0;
-	
-	while(i < ac)
+
+	while(**av)
 	{
-	cout << *av << endl;
-	*av[i++];
+	cout << ac << endl;
+	cout << **av << endl;
+	// cout << *av << endl;
+	// *av[i++];
 	}
 }
