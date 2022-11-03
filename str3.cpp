b22d38a4b3c4d2bd1d0777110e82a7120fe3eeba
root <root@DESKTOP-OKSHI5U.localdomain>
2022-11-03 17:12:30 +0900

    ++++

--- str3.cpp
+++ str3.cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <cstring>
 // #include <locate>
+
+
+size_t ft_strlen(char *s)
+{
+    int i;
+    i = 0;
+    while(s)
+    {
+        s++;
+        i++;
+    }
+        return(i);
+}
 
 char ft_toupper(char c)
 {
@@ -11,21 +25,31 @@ char ft_toupper(char c)
 int main (int ac, char **av)
 {
     int i,j;
-    i = 1;
+    i = 0;
     j = 0;
 
     // std::cout << "1";
     if (ac == 1)
     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     else
+
     while(i < ac)
     {
+    std::cout << i << std::endl;
     std::string str(av[i]);
     i++;
-    j++;
     std::cout << str << std::endl;
-    std::cout << str[i][j] << std::endl;
+    while(j < strlen(*av))
+    {
+    std::string str2;
+    // int m;
+    str2 = ft_toupper(str[j]);
+    // std::cout << str[j] <<std:: endl;
+    std::cout << str2;
+    // std::cout << str[i][j] << std::endl;
+    // std::cout << ft_toupper(av[i][j]) << std::endl;
+    j++;
+    }
 
     }
-    // n = ft_toupper(m);
 }
---
+++ study/toupper+++.cpp
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <iostream>
+
+using  namespace::std;
+ int main (int ac , char **av)
+ {
+int i , j;
+int m[] = {1, 2 ,3};
+i = 0;
+j =0;
+ const char *s = "dfnakl";
+std::string str;
+std::string str2;
+str2 = toupper(av[i]);
+std::cout << str2 <<std:: endl;
+printf("%c\n", m[j]);
+ }
