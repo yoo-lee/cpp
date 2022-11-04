8fa2a3a3b040e4a9d3edfa390fce6eeffb7710bb
root <root@DESKTOP-OKSHI5U.localdomain>
2022-11-04 18:41:38 +0900

    ++++++

--- str3.cpp
+++ str3.cpp
@@ -2,8 +2,6 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-// #include <locate>
-
 
 size_t ft_strlen(char *s)
 {
@@ -14,42 +12,43 @@ size_t ft_strlen(char *s)
         s++;
         i++;
     }
-        return(i);
+    return(i+1);
 }
 
 char ft_toupper(char c)
 {
-    return(c - ('Z' - 'z'));
+    return(c + ('Z' - 'z'));
 }
 
 int main (int ac, char **av)
 {
     int i,j;
-    i = 0;
-    j = 0;
+    i = 1;
 
-    // std::cout << "1";
     if (ac == 1)
     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     else
 
     while(i < ac)
     {
-    std::cout << i << std::endl;
+    j = 0;
+    std::cout << "i ="<< i <<std::endl;
     std::string str(av[i]);
-    i++;
     std::cout << str << std::endl;
-    while(j < strlen(*av))
-    {
-    std::string str2;
-    // int m;
-    str2 = ft_toupper(str[j]);
-    // std::cout << str[j] <<std:: endl;
-    std::cout << str2;
-    // std::cout << str[i][j] << std::endl;
-    // std::cout << ft_toupper(av[i][j]) << std::endl;
-    j++;
+    std::cout << std::endl;
+        i++;
+        while(j < str.length())
+        {
+        std::string str2;
+        // std::cout << "j =" << j <<std::endl;
+        str2 = (char)ft_toupper(str[j]);
+        // std::cout << str[j] <<std:: endl;
+        j++;
+        std::cout << str2;
+        // std::cout << str[i][j] << std::endl;
+        // std::cout << ft_toupper(av[i][j]) << std::endl;
+        }
+        std::cout << std::endl;
     }
-
-    }
+        return(0);
 }
