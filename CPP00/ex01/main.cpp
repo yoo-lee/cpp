03b740764f90e0e97313b4f2bf2bec94ae34050a
yoo-lee <83053630+yoo-lee@users.noreply.github.com>
2022-11-14 19:51:02 +0900

    1115

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -1,24 +1,25 @@
-
 #include <iostream>
 
 int main ()
 {
 	std::string str;
-	Phonebook	phonebook;
+	// Phonebook	phonebook;
 	
 	while (1)
 	{
 		std::cout << "Enter command: ";
 		std::cin >> std::ws;
-	}
-    if (std::cin.eof())
-	{
-		std::cout << '\n';
+		std::getline(std::cin, str);
+		if (std::cin.eof())
+		{
+			std::cout << '\n';
+			std::cin.clear();
+			std::clearerr(stdin);
+		}
+		else if (str == "ADD")
+			phonebook.addcontact();
+		else 
+			std::cout << "command not found\n";
 	}
 	return(0);
-} 
-   
-
-
-
 }
