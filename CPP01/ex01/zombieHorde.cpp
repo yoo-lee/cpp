e506d2fa6d77e8e6cfa22b5291d3f544fccd6b9f
root <root@DESKTOP-OKSHI5U.localdomain>
2022-11-25 18:41:11 +0900

    +++

--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -20,30 +20,33 @@ Zombie::Zombie(string name)
 	z_name = name;
 }
 
-Zombie* Zombie::zombieHorde( int N, std::string name )
-{
-	Zombie *zombieHorde = new Zombie[N];
 
-	for( int i = 0; i < N; ++i)
-	{
-		
-	}
-	Zombie (name);
-	zombieHorde[i].setZombieName(name + '_' + string) = new char(N);
-	return (zom);
-}
-
-int main ()
-{
-	
-
-}
 // It must allocate N Zombie objects in a single allocation.
 //  Then, it has to initialize the
 // zombies, giving each one of them
 //  the name passed as parameter.
 //  The function returns a
 // pointer to the first zombie.
-// Implement your own tests to ensure your zombieHorde() 
+// Implement your own tests to ensure your zombieHorde()
 // function works as expected.
 // Try to call announce() for each one of the zombies.
+Zombie* Zombie::zombieHorde( int N, std::string name )
+{
+	Zombie*  zombieHorde = new Zombie[N];
+	for( int i = 0; i < N; ++i)
+	{
+		zombieHorde[i].setZombieName(name + '_' + std::to_string(i));
+	}
+	return (zombieHorde);
+}
+
+int main(void)
+{
+	int N = 10;
+	Zombie* zombies = zombieHorde(N,"Zombie");
+
+	for (int i = 0; i < N; i++)
+		zombies[i].announce();
+	delete[] zombies;
+	return (0);
+}
