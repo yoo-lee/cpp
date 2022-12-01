8e45091ff3c5f53fe115bd6155f51107ad27a326
Yoo-Lee <yuuuukan@gmail.com>
2022-12-01 15:10:56 +0900

    r

--- CPP01/ex03/Weapon.h
+++ CPP01/ex03/Weapon.h
@@ -1,13 +1,21 @@
+#ifndef WEAPON_H
+#define WEAPON_H
+#include <iostream>
+
 class Weapon
 {
 private:
     std::string type;
     
 public:
-    getType(); 
-    setType();
-    ~();
-// };
+    Weapon();
+    ~Weapon();
+    
+    std::string const getType(); 
+    void    setType(std::string type);
+};
+
+#endif
 // Implement a Weapon class that has:
 // • A private attribute type, which is a string.
 // • A getType() member function that returns a const reference to type.
