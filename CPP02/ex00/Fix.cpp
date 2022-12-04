1d0787427a4fa2350c69338cfcfe168f451bf8d0
root <root@DESKTOP-OKSHI5U.localdomain>
2022-12-04 12:03:33 +0900

    pp

---
+++ CPP02/ex00/Fix.cpp
@@ -0,0 +1,43 @@
+#include "Fix.h"
+
+    Fixed::Fixed()
+    {
+        std::cout << "Default constructor called" << std::endl;
+        this -> Fixeded_point_number = 0;
+    }
+
+    Fixed::Fixed(const Fixed& fix)
+    {
+        std::cout << "Copy constructor called" << std::endl;
+        *this = fix;
+    }
+
+// fixed to int
+    int Fixed::getRawBits(void) const
+    {
+	    std::cout << "getRawBits member function called\n";
+	    return (this->fractionalBits);
+    }
+
+    Fixed::~Fixed()
+    {
+        std::cout << "deconstructor" << std::endl;
+    }
+    void Fixed::setRawBits( int const raw )
+    {
+        Fixeded_point_number = raw;
+    }
+
+//  Private members:
+// ◦ An integer to store the fixed-point number value.
+// ◦ A static constant integer to store the number of fractional bits. Its value
+// will always be the integer literal 8.
+// • Public members:
+// ◦ A default constructor that initializes the fixed-point number value to 0.
+// ◦ A copy constructor.
+// ◦ A copy assignment operator overload.
+// ◦ A destructor.
+// ◦ A member function int getRawBits( void ) const;
+// that returns the raw value of the fixed-point value.
+// ◦ A member function void setRawBits( int const raw );
+// that sets the raw value of the fixed-point number.
--- CPP02/ex00/Fix.h
+++ CPP02/ex00/Fix.h
@@ -1,33 +1,28 @@
 #ifndef FIX_H
-#define FIX_H
+# define FIX_H
+
 #include <iostream>
 
 class Fixed
 {
-    Private:
+    private:
 // ◦ An integer to store the Fixeded-point number value.
-    int Fixeded_point_number
-    const int fractionalBits = '8';
+    int Fixeded_point_number;
+    static const int fractionalBits = '8';
 
-    Public:
+    public:
     Fixed();
-    Fixed(Fixed &fixed);
+    Fixed(const Fixed& fixed);
     ~Fixed();
-// public:
-// 	Fixed();
-// 	Fixed (Fixed &fixed);
-// 	~Fixed();
-// 	Fixed	&operator=(Fixed &fixed);
-
-// 	int		getRawBits(void) const;
-// 	void	setRawBits(int raw);
-
-
-// ◦ A member function int getRawBits( void ) const;
-    int getRawBits( void );
-// that returns the raw value of the Fixeded-point value.
+    int getRawBits(void) const;
 
 // ◦ A member function void setRawBits( int const raw );
 // that sets the raw value of the Fixeded-point number.
     void setRawBits( int const raw );
-}
+
+// ◦ A member function int getRawBits( void ) const;
+// that returns the raw value of the Fixeded-point value.
+
+};
+
+#endif
