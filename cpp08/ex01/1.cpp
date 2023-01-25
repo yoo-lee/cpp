2f65943a43467489faf16c7db7c59ad95e844a71
Yoo-Lee <yuuuukan@gmail.com>
2023-01-25 15:40:39 +0900

    +

--- cpp08/ex01/1.cpp
+++ cpp08/ex01/1.cpp
@@ -3,14 +3,20 @@
 
 int main ()
 {
- std::multiset<int> st{8, 4, 1, 4} ;
-    // std::multiset<int>::iterator it = st.begin();
-	auto it = st.end();
+ std::multiset<int> st{ 4, 1, 4} ;
+    std::multiset<int>::iterator it = st.begin();
+	std::multiset<int>::iterator a = st.end(); 
+	// auto it = st.end();
 	std::cout << *it << std::endl;
-	for (auto it = st.begin(); it != st.end(); ++it)
+	std::cout << *a << std::endl;
+	for (auto i = st.begin(); i != st.end(); ++i)
 	{
-		std::cout << *it << std::endl;
+		std::cout << *i <<std::endl;
 	}
+	// for (auto it = st.begin(); it != st.end(); ++it)
+	// {
+	// 	std::cout << *it << std::endl;
+	// }
 	// for(auto itr = st.begin(); itr != st.end(); ++itr)
 	//  {
     //     std::cout << *itr << "\n";      // イテレータの指す先のデータを表示
