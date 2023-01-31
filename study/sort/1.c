e4b0c4aaa3e01a57dc401b155aceb920f84a637b
yoo-lee <83053630+yoo-lee@users.noreply.github.com>
2023-02-01 00:07:10 +0900

    ++

---
+++ study/sort/1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+	
+void sort(int a[])
+{
+	int i,j;
+	i = 0;
+	 
+	while(i < 5)
+	{
+		int j = i+1;
+		while(j < 5)
+		{
+		if(a[j] <a[i])	
+			{
+			int tmp = a[i];
+			a[i] = a[j];
+			a[j] = tmp;
+			}
+		j++;
+		}
+	i++;
+	}
+}
+
+int main (void)
+{
+	int a[5] = {5,2,8,9,1};
+	int i = 0;
+	sort(a);
+	
+	while(i < 5)
+	{
+	printf("%d",a[i]);
+	i++;
+	}
+	return 0;
+}
